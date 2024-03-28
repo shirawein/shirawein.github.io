82e0f40f4ac8647ae4a35d12bedcf9b298f9adbd
shirawein <sbmw15@gmail.com>
2024-03-28 11:43:02 -0400

    Update and rename publications.html to publications.php

---
+++ publications.php
@@ -0,0 +1,7 @@
+<h3>hello!</h3>
+
+<!-- <script src="https://bibbase.org/show?bib=https://shirawein.github.io/pubs.bib&jsonp=1"></script>
+ -->
+ <?php $contents =
+              file_get_contents("https://bibbase.org/show?bib=https://shirawein.github.io/pubs.bib&jsonp=1");
+              print_r($contents); ?> 
